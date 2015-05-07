db33460085fba0329dd5ccfef60dae00f75668e9
Matt Dennewitz <mattdennewitz@gmail.com>
2015-05-07 16:57:59 -0500

    toward context

--- mattreduce/job.py
+++ mattreduce/job.py
@@ -2,11 +2,14 @@ import collections
 
 
 class Job(object):
+    """An executable container for the map/reduce/finalize workflow.
+    """
 
-    def __init__(self, mapper, reducer, finalizer=None):
+    def __init__(self, mapper, reducer, finalizer=None, context=None):
         self.mapper = mapper
         self.reducer = reducer
         self.finalizer = finalizer
+        self.context = context
 
     def combine(self, mapped_values):
         """Group mapped <key, value> pairs.
@@ -25,11 +28,15 @@ class Job(object):
 
     def run(self, items):
         # map
-        mapped = (self.mapper(item) for item in items)
+        mapped = (self.mapper(item, context=self.context) for item in items)
         # combine
         combined = self.combine(mapped)
         # reduce
-        reduced = (self.reducer(key, values) for key, values in combined)
+        reduced = (
+            self.reducer(key, values, context=self.context)
+            for key, values
+            in combined
+        )
 
         if self.finalizer is None:
             return reduced
